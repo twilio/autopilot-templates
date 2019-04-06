62d1b2c4ea82c89357fee2b524ab50018bd9b83c
Nicolas Acosta <acossta@gmail.com>
2019-04-05 17:42:34 -0700

    updates  to send to autopilot function
    updates  to send to autopilot function

--- Functions/autopilot-studio-chat/autopilot-studio-chat.js
+++ Functions/autopilot-studio-chat/autopilot-studio-chat.js
@@ -5,7 +5,7 @@ exports.handler = function(context, event, callback) {
     // Takes the first message, creates the channel webhook
     console.log(event);
     
-     //callback(null);
+     //
     let channelSid = event.channelSid;
     
     const client = context.getTwilioClient();
@@ -14,12 +14,12 @@ exports.handler = function(context, event, callback) {
     // Pipe body to Autopilot
     let requestPayload = "user_id="+event.from+"&text="+event.text;
 
-    got.post('https://channels.autopilot.twilio.com/v1/'+context.TWILIO_ACCOUNT_SID+'/'+context.TWILIO_AUTOPILOT_ASSISTANT_SID+'/custom/webchat', 
+    got.post('https://channels.autopilot.twilio.com/v1/'+context.ACCOUNT_SID+'/'+context.TWILIO_AUTOPILOT_ASSISTANT_SID+'/custom/webchat', 
     { 
         headers: { 
             'content-type': 'application/x-www-form-urlencoded',
             'accept': 'application/json',
-            'authorization' :  'Basic ' + new Buffer(context.TWILIO_ACCOUNT_SID+ ':' + context.AUTH_TOKEN).toString('base64')
+            'authorization' :  'Basic ' + new Buffer(context.ACCOUNT_SID+ ':' + context.AUTH_TOKEN).toString('base64')
         },
         body: requestPayload
     }).then(function(response) {
@@ -30,22 +30,18 @@ exports.handler = function(context, event, callback) {
         console.log("send the message");
         let saysArray = apResponse.says;
         let saysProcessed = 0;
+        let responseMessage = "";
         
         saysArray.forEach(function(value){
-            console.log(value.speech);
-            service.channels(channelSid).messages.create({"body": value.speech, "from": "Autopilot"})
+            responseMessage += " "+ value.speech;
+        });
+        service.channels(channelSid).messages.create({"body": responseMessage, "from": "Autopilot"})
             .then((response) => {
                 console.log("Reponse:"+response);
-                // Set the Channel Webhook
-                // TODO
-                saysProcessed++;
-                if(saysProcessed === saysArray.length) {
-                    callback(null, {destimnation:"autopilot"});
-                }
+                callback(null, {destination:"autopilot"});
             }).catch(error => { 
                 console.log(error); 
                 callback(error); });
-        });
         
     }).catch(function(error) {
         callback(error)
