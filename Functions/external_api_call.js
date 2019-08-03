3dd9d644a4bb95d4cd558daffa939168817ead53
Nicolas Acosta <acossta@gmail.com>
2019-08-03 10:54:19 -0700

    remove request payload

--- Functions/external_api_call.js
+++ Functions/external_api_call.js
@@ -6,13 +6,13 @@ exports.handler = function(context, event, callback) {
     console.log("User Identifier: "+ event.UserIdentifier);
     console.log("Task: "+ event.CurrentTask);
     console.log(event);
+    
 
     got.post('https://icanhazdadjoke.com/', 
     { 
         headers: { 
             'accept': 'application/json'
-        },
-        body: requestPayload
+        }
     }).then(function(response) {
         let apResponse = JSON.parse(response.body);
         console.log(apResponse);
